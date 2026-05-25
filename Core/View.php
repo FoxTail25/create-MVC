9ad28c7f833d31d151b86a348a6bd8f7a6ac5a6c
Fox Tail <foxtail25@gmail.com>
2026-05-25 11:51:12 +0300

    добавил описания

--- Core/View.php
+++ Core/View.php
@@ -1,38 +1,57 @@
 <?php
-	namespace Core;
-	
-	class View
+
+namespace Core;
+
+/*
+	Класс View (представление), формирует финальный облик страницы, которая будет возвращена пользователю по его запросу.
+	*/
+
+class View
+{
+	/*
+	Возвращаем готовую для отображения страницу
+	*/
+	public function render(Page $page)
 	{
-		public function render(Page $page) {
-			return $this->renderLayout($page, $this->renderView($page));
+		return $this->renderLayout($page, $this->renderView($page));
+	}
+
+	/*
+	Вставляем представление (View), "внутрь" Layout
+	*/
+	private function renderLayout(Page $page, $content)
+	{
+		$layoutPath = $_SERVER['DOCUMENT_ROOT'] . "/project/layouts/{$page->layout}.php";
+
+		if (file_exists($layoutPath)) {
+			ob_start();
+			$title = $page->title;
+			include $layoutPath;
+			return ob_get_clean();
+		} else {
+			echo "Layout file not found at path $layoutPath";
+			die();
 		}
-		
-		private function renderLayout(Page $page, $content) {
-			$layoutPath = $_SERVER['DOCUMENT_ROOT'] . "/project/layouts/{$page->layout}.php";
-			
-			if (file_exists($layoutPath)) {
+	}
+
+	/*
+	Вставляем данные, "внутрь" предствления (View).
+	*/
+	private function renderView(Page $page)
+	{
+		if ($page->view) {
+			$viewPath = $_SERVER['DOCUMENT_ROOT'] . "/project/views/{$page->view}.php";
+
+			if (file_exists($viewPath)) {
 				ob_start();
-					$title = $page->title;
-					include $layoutPath;
+				$data = $page->data;
+				extract($data);
+				include $viewPath;
 				return ob_get_clean();
 			} else {
-				echo "Layout file not found at path $layoutPath"; die();
-			}
-		}
-		
-		private function renderView(Page $page) {
-			if ($page->view) {
-				$viewPath = $_SERVER['DOCUMENT_ROOT'] . "/project/views/{$page->view}.php";
-				
-				if (file_exists($viewPath)) {
-					ob_start();
-						$data = $page->data;
-						extract($data);
-						include $viewPath;
-					return ob_get_clean();
-				} else {
-					echo "View file not found at path $viewPath"; die();
-				}
+				echo "View file not found at path $viewPath";
+				die();
 			}
 		}
 	}
+}
